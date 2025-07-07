8e32061fca750c212484d9de8c548b4992bc4345
Chamara Fernando <Chamarafernando2428@gmail.com>
2025-07-07 20:22:54 +0530

    dht 11 update

--- utils/ventilation.py
+++ utils/ventilation.py
@@ -1,33 +1,28 @@
-# utils/ventilation.py
-
 import board
 import adafruit_dht
+import time
 
 _ventilation_on = False
-
 dhtDevice = adafruit_dht.DHT11(board.D4)
+_last_read = 0
+_last_temp = None
+_last_hum = None
 
 def get_env_status():
-    try:
-        temperature = dhtDevice.temperature
-        humidity = dhtDevice.humidity
-        return {
-            'temperature': temperature,
-            'humidity': humidity,
-            'ventilation': "ON" if _ventilation_on else "OFF"
-        }
-    except RuntimeError:
-        return {
-            'temperature': None,
-            'humidity': None,
-            'ventilation': "ON" if _ventilation_on else "OFF"
-        }
-
-def control_ventilation():
-    env = get_env_status()
-    global _ventilation_on
-    if env['humidity'] and env['humidity'] > 70 or env['temperature'] and env['temperature'] > 30:
-        _ventilation_on = True
+    global _last_read, _last_temp, _last_hum
+    now = time.time()
+    if now - _last_read > 2:
+        try:
+            _last_temp = dhtDevice.temperature
+            _last_hum = dhtDevice.humidity
+        except RuntimeError as e:
+            print("DHT11 read error:", e)
+        _last_read = now
+    return {
+        'temperature': _last_temp,
+        'humidity': _last_hum,
+        'ventilation': "ON" if _ventilation_on else "OFF"
+    }
 
 def toggle_ventilation():
     global _ventilation_on
